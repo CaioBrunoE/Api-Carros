4fb1207f93f84f32ac63be7b3ce64ecb86cc2d24
Caio Bruno <caiobruno90@yahoo.com>
2024-01-18 13:42:05 -0300

    Implementado testeSave

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -2,21 +2,50 @@ package com.example.carros;
 
 import com.example.carros.domain.Carro;
 import com.example.carros.domain.CarroService;
+import com.example.carros.domain.dto.CarroDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CarrosApplicationTests {
     @Autowired
 	private CarroService service;
 	@Test
-	void teste1() {
+	void testeSave() {
 		Carro carro = new Carro();
 		carro.setNome("Ferrari");
-		carro.setTipo("Esportivo");
+		carro.setTipo("esportivo");
 
-		service.insert(carro);
+		CarroDTO c = service.insert(carro);
+
+		assertNotNull(c);
+
+		Long id = c.getId();
+
+		assertNotNull(id);
+
+		//Buscar o objeto
+		Optional<CarroDTO> op = service.getCarroById(id);
+		assertTrue(op.isPresent());
+
+		c = op.get();
+		assertEquals("Ferrari", c.getNome());
+		assertEquals("esportivo", c. getTipo());
+
+
+       //Deletar objeto
+		service.delete(id);
+
+		//Verificar se foi deletado
+		assertFalse(service.getCarroById(id).isPresent());
+
+
+
 	}
 
 	@Test
