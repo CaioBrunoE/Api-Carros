24495369ef92119fb75eb2253552c227c15b930a
Caio Bruno <caiobruno90@yahoo.com>
2024-01-18 13:13:27 -0300

    Criando primeiro teste unitario

--- carros/src/test/java/com/example/carros/CarrosApplicationTests.java
+++ carros/src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.carros;
 
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarrosApplicationTests {
+    @Autowired
+	private CarroService service;
+	@Test
+	void teste1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("Esportivo");
+
+		service.insert(carro);
+	}
 
 	@Test
 	void contextLoads() {
+
+
 	}
 
 }
